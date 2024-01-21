7c794f6b5368df72681392fc58f1ea73601a5f78
teja7 <teja545damera@gmail.com>
2024-01-21 23:51:58 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ToastModule } from 'primeng/toast';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MessageService } from 'primeng/api';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
