9c76f6845251f9e4719ab8bcff8eda8c64836374
teja7 <teja545damera@gmail.com>
2024-01-21 13:00:35 +0530

    test

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  constructor(private fb:FormBuilder){}
+  loginForm=this.fb.group({
+    email:['',[Validators.required,Validators.email]],
+    password:['',[Validators.required]]
+  })
+  
+} 
