69f0b3363be4d329a5f91a74f1a81f0445df1720
teja7 <teja545damera@gmail.com>
2024-01-21 23:25:58 +0530

    toast message

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { passwordMatchValidator } from '../../shared/password-match.directive';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interfaces/auth';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -17,7 +21,11 @@ export class RegisterComponent {
   }, {
     validators: passwordMatchValidator
   })
-  constructor(private fb:FormBuilder){}
+  constructor(private fb:FormBuilder, 
+    private authService:AuthService, 
+    private messageService:MessageService, 
+    private router:Router
+    ){}
   get fullName(){
     return this.registerForm.controls['fullName'];
   }
@@ -29,5 +37,19 @@ export class RegisterComponent {
   }
   get confirmPassword(){
     return this.registerForm.controls['confirmPassword'];
+  }
+  submitDetails(){
+    const postData={...this.registerForm.value};
+    delete postData.confirmPassword;
+    this.authService.registerUser(postData as User).subscribe(
+      response=>{
+        console.log(response),
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Register Successfully' });
+        this.router.navigate(['login'])
+      },
+      error=>{
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong' });
+      }
+  )
   }
 }
